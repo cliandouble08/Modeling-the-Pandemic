0e38ed9a03b569f36a2734d739687e5088b81a3b
Shonda Kuiper <stat2labs@gmail.com>
2023-12-14 09:36:33 -0600

    updated descriptions

--- build/main.R
+++ build/main.R
@@ -3,5 +3,4 @@
 source("./dep/package-prep-main.R")
 
 # Data prep
-# This has several additional packages used for cleaning, not needed to run the app.
 df <- read_csv("data/merged_processed_data.csv")
