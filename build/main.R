c2acac2433d33ca47447f06a28df6961a761ad48
Shonda Kuiper <stat2labs@gmail.com>
2023-12-14 09:27:58 -0600

    update descriptions

--- build/main.R
+++ build/main.R
@@ -1,5 +1,7 @@
 # Package prep
+# This is for running the analysis, not the cleaning.
 source("./dep/package-prep-main.R")
 
 # Data prep
+# This has several additional packages used for cleaning, not needed to run the app.
 df <- read_csv("data/merged_processed_data.csv")
